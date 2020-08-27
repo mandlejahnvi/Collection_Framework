2c21e24819a2781cf93798e8a0ced3224f1529bb
Jahnvi Mandle <67870408+mandlejahnvi@users.noreply.github.com>
2020-08-27 12:13:57 +0530

    Create LinkedExp2.java

---
+++ LinkedExp2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/**
+ *
+ * @author jahnvi
+ */
+public class LinkedExp2 {
+    public static void main(String args[])
+    {
+        List <String> a=new LinkedList<String> ();
+        a.add("node1");
+        a.add("node2");
+        a.add("node3");
+        for(String str:a)
+        {
+            System.out.println(str);
+        }
+    }
+}
